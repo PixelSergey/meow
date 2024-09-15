1ce70616873b2f26bbf8eb7e5a66c51aa5f8bf7c
PixelSergey <sergey.ichtchenko@gmail.com>
2024-09-16 01:52:09 +0300

    Add clap for parsing command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+/// Print ASCII cats to your terminal
+struct Args {
+    /// How many cats to print
+    #[arg(short = 'c', long = "count", default_value_t = 1)]
+    count: u8,
+
+    /// Are you literally this cat?
+    #[arg(short = 'l', long = "literally", action)]
+    literally: bool
+}
+
 fn main() {
+    let args = Args::parse();
+
     println!("Hello, world!");
 }
